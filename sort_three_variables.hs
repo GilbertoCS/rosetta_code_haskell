281d72b1309f9070e3ce546e72d5d7256de6d370
GilbertoCS <GilbertoCS@users.noreply.github.com>
2017-11-22 21:09:27 -0200

    Second attempt to solve the three variables problem

--- sort_three_variables.hs
+++ sort_three_variables.hs
@@ -12,16 +12,36 @@
 
 import Data.List
 
--- test 1
-x = "lions, tigers, and"
-y = "bears, oh my!"
-z = "(from the 'Wizard of OZ')"
-
 sortThree :: (Ord a) => a -> a -> a -> [a]
 sortThree x y z = sort (threeVariablesToList x y z)
 
-threeVariablesToList :: a -> a-> a -> [a]
+threeVariablesToList :: a -> a -> a -> [a]
 threeVariablesToList variable1 variable2 variable3 = [variable1, variable2, variable3]
 
--- show sortThree x y z
-                                
+test1 = showVariables l
+            where x = "lions, tigers, and"
+                  y = "bears, oh my!"
+                  z = "(from the 'Wizard of OZ')"
+                  l = sortThree x y z                
+
+                  --TODO solucao falha para numeros
+--showVariables :: [a] -> IO ()
+showVariables [x,y,z] = print ("X = " ++ x ++ " Y = " ++ y ++ " Z = " ++ z)
+
+-- direct solution with do notation
+
+solution2 = do
+      let x = "lions, tigers, and"
+      putStrLn ("X = " ++ x)
+      let y = "bears, oh my!"
+      putStrLn ("Y = " ++ y)
+      let z = "(from the 'Wizard of OZ')"
+      putStrLn ("Z = " ++ z)
+      let listSorted = sort [x,y,z]
+      let x = head listSorted
+      putStrLn (" ------------- ")
+      putStrLn ("X = " ++ x)
+      let y = listSorted !! 1
+      putStrLn ("Y = " ++ y)
+      let z = listSorted !! 2
+      putStrLn ("Z = " ++ z)

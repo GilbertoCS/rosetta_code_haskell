ed19b263b5221a89edbe290c387f9916a5b77fc5
GilbertoCS <GilbertoCS@users.noreply.github.com>
2017-11-25 20:08:11 -0200

    More two rules implementeds

--- natural_sort.hs
+++ natural_sort.hs
@@ -2,31 +2,38 @@
 
 import Data.List
 import Data.Char
--- 1. Ignore leading, trailing and multiple adjacent spaces
 
-{--
-Text strings:
-['ignore leading spaces: 2-2',
- ' ignore leading spaces: 2-1',
- '  ignore leading spaces: 2+0',
- '   ignore leading spaces: 2+1']
-Normally sorted :
-['   ignore leading spaces: 2+1',
- '  ignore leading spaces: 2+0',
- ' ignore leading spaces: 2-1',
- 'ignore leading spaces: 2-2']
-Naturally sorted:
-['  ignore leading spaces: 2+0',
- '   ignore leading spaces: 2+1',
- ' ignore leading spaces: 2-1',
- 'ignore leading spaces: 2-2']
- --}
+-- TODO gerar output com o valor original organizadoe nao somente o novo valor organizado
+
+-- 1. Ignore leading, trailing and multiple adjacent spaces
 
  -- Ignoring leading spaces
 sampleFirstRule = ["ignore leading spaces: 2-2", " ignore leading spaces: 2-1", "  ignore leading spaces: 2+0",  "   ignore leading spaces: 2+1"]
 
-ignoringLeadingSpaces :: String -> String
-ignoringLeadingSpaces = dropWhile isSpace
+removeLeadingSpaces :: String -> String
+removeLeadingSpaces = dropWhile isSpace
 
-sortFirstRule :: [String] -> [String]
-sortFirstRule l = sort $ map ignoringLeadingSpaces l
+sortIgnoringLeadingSpaces :: [String] -> [String]
+sortIgnoringLeadingSpaces l = sort $ map removeLeadingSpaces l
+
+-- Ignoring multiple adjacent spaces and Equivalent whitespace characters
+sampleSecondRule = ["ignore m.a.s spaces: 2-2", "ignore m.a.s  spaces: 2-1", "ignore m.a.s   spaces: 2+0", "ignore m.a.s    spaces: 2+1"]
+
+sortIgnoringMultipleAdjacentSpaces :: [String] -> [String]
+sortIgnoringMultipleAdjacentSpaces l = sort $ map (unwords . words) l
+
+-- Equivalent whitespace characters
+sampleThirdRule = ["Equiv. spaces: 3-3", "Equiv.\rspaces: 3-2", "Equiv.\x0cspaces: 3-1", "Equiv.\x0bspaces: 3+0", "Equiv.\nspaces: 3+1", "Equiv.\tspaces: 3+2"]
+
+sampleFourthRule = ["cASE INDEPENENT: 3-2", "caSE INDEPENENT: 3-1", "casE INDEPENENT: 3+0", "case INDEPENENT: 3+1"]
+{--
+val r3 = Regex("""\s""")  // \s represents any whitespace characterval r5 = Regex("""\d+""")
+ 
+
+ 
+/** Case independent sort */
+fun selector4(s: String) = s.toLowerCase()
+ 
+/** Numeric fields as numerics (deals with up to 20 digits) */ 
+fun selector5(s: String) = r5.replace(s) { it.value.padStart(20, '0') }
+--}

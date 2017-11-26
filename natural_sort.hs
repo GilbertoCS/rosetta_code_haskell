4d8c19d57b5dbe5ae89048132512190693950cdf
GilbertoCS <GilbertoCS@users.noreply.github.com>
2017-11-26 20:52:40 -0200

    4 rule implemented

--- natural_sort.hs
+++ natural_sort.hs
@@ -25,14 +25,19 @@ sortIgnoringMultipleAdjacentSpaces l = sort $ map (unwords . words) l
 -- Equivalent whitespace characters
 sampleThirdRule = ["Equiv. spaces: 3-3", "Equiv.\rspaces: 3-2", "Equiv.\x0cspaces: 3-1", "Equiv.\x0bspaces: 3+0", "Equiv.\nspaces: 3+1", "Equiv.\tspaces: 3+2"]
 
+--Case independent sort
 sampleFourthRule = ["cASE INDEPENENT: 3-2", "caSE INDEPENENT: 3-1", "casE INDEPENENT: 3+0", "case INDEPENENT: 3+1"]
+
+sortCaseIndependent :: [String] -> [String]
+sortCaseIndependent l = sort $ map (map toLower) l
+
+-- Numeric fields as numerics (deals with up to 20 digits) 
+sampleFifthRule = ["foo100bar99baz0.txt", "foo100bar10baz0.txt", "foo1000bar99baz10.txt", "foo1000bar99baz9.txt"]
+
 {--
 val r3 = Regex("""\s""")  // \s represents any whitespace characterval r5 = Regex("""\d+""")
  
 
- 
-/** Case independent sort */
-fun selector4(s: String) = s.toLowerCase()
  
 /** Numeric fields as numerics (deals with up to 20 digits) */ 
 fun selector5(s: String) = r5.replace(s) { it.value.padStart(20, '0') }
